d89f9ea7cb9544cbeb74df989104382de463619c
Spleefies <cutecatty32@gmail.com>
2026-03-01 16:08:45 +0800

    oops

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "static",
+  base: "/static/",
   title: "Home",
   titleTemplate: ':title',
   description: "Static site",
